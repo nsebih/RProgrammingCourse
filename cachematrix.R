0616df62a029ad9e7e826861e410e5b0730a9792
Nazim Sebih <nazimsebih@Nazims-MacBook-Air.local>
2015-06-21 23:47:30 +0900

    Changeset destined for assignment 2 submission Committer: Nazim Sebih <nazimsebih@Nazims-MacBook-Air.local> On branch master Changes to be committed: 	modified:   cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The two functions participate to solve the inverse of a matrix (provided it is squared), and store in cache the result for subsequent calls to computation of the inverse of the same matrix. Every time a net matrix is sat, inverse is reinitialized to null values.
 
-## Write a short comment describing this function
-
+## Provides getters and setters for the matrix, return an object (list) of these functions.
+	
 makeCacheMatrix <- function(x = matrix()) {
-
+	#reject a matrix that is not square; non-square matrices can only have a pseudo-inverse or best approximation
+	if(nrows(x)!=ncols(x))
+		return "Error: non square matrix"
+	
+	#in case function's arguments is square:
+	r<-nrows(x)
+	c<- ncols(x)
+	#iniialize the inverse matrix of argument to null values
+	inverse <- matrix(NULL, r,c)	
+	
+	setMatrix <- function(y){
+		x <<- y
+		r<-nrows(x)
+		c<- ncols(x)
+		#perform same check for dimension of the matrix, in case it is set with setMatrix function:
+		if(nrows(x)!=ncols(x))
+			return "Error: non square matrix"
+		#reinitilize matrix inverse to null values
+		inverse <<- matrix(NULL, r,c)
+	}
+	
+	getMatrix <- function() x
+	
+	setMatrixInverse <- function(i) inverse <<-i
+	getMatrixInverse <- function() inverse
+	#return an object consisting of setters/getters for matrix and its inverse
+	list(setMatrix= setMatrix, getMatrix=getMatrix, setMatrixInverse=setMatrixInverse, getMatrixInverse=getMatrixInverse)
+		
 }
 
 
-## Write a short comment describing this function
+## Retrieve the inverse of a matrix if already cached, otherwise compute it store it and return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+		inverse <- x$getMatrixInverse()
+		
+		if(inverse[1][1]!=NULL){
+			#if the inverse has already been computed 
+			return inverse
+		}
+		else{
+			#if inverse has not yet been computed, solve it, cache it then return it
+			matrix <-x$getMatrix()
+			inverse <- solve(matrix)
+			x$setInverse(inverse)
+			return inverse()	
+		}
+
 }
